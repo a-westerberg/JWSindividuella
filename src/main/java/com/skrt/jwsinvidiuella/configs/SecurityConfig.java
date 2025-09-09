a6e6c0b8a93fa6739188bf571a05cad00381e129
aaalx <alexander.westerberg.svedberg@gmail.com>
2025-09-09 11:22:34 +0200

    JwtAuthConverter och SecurityConfig

--- src/main/java/com/skrt/jwsinvidiuella/configs/SecurityConfig.java
+++ src/main/java/com/skrt/jwsinvidiuella/configs/SecurityConfig.java
@@ -1,4 +1,37 @@
 package com.skrt.jwsinvidiuella.configs;
 
+import com.skrt.jwsinvidiuella.converters.JwtAuthConverter;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configurable
+@EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthConverter jwtAuthConverter;
+
+
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .headers(h -> h.frameOptions(f -> f.disable()))
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth->oauth.jwt(j -> j.jwtAuthenticationConverter(jwtAuthConverter))
+                );
+        return http.build();
+    }
+
 }
